57711e74a7ddd3f77ad867f920d4e555e5ca225b
Limshuc <141672693+Limshuc@users.noreply.github.com>
2024-06-15 18:24:04 +0800

    Add files via upload

---
+++ Data Science Tools and Ecosystem (1).py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Title is "Data Science Tools and Ecosysyem"
+
+# There are nummerous Data Science Languages but the most common few are 
+# Python, Java, C++, R, SQL and Java Script
+
+# Common Data Science libs are
+# NumPy, SciPy, Matplotlib, Seaborn, Scikit-learn and Pandas
+
+# Data Science Tools like 
+# 
+# Anaconda
+# Jupyter Notebook
+# R Studio
+# Spyder
+# Zeppelin
+
+# a = 7
+# b= 2
+# print (a*b)
+
+# In[9]:
+
+
+print((3*4)+5)
+
+
+# In[13]:
+
+
+200//60
+
+
+
+# Add Comments to code cells
+# Create Markdown Cells
+# Add Ordered and unordered lists to markdown cells
+# Format Content of markdown cells with heading styles
+
+# Author Name
+# Matt
